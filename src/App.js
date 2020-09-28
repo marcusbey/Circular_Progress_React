f2c1c150c1bd729b4612821a6dc49d6f1f6a9c18
Romain BOBOE <rboboe@gmail.com>
2020-09-28 18:55:34 -0400

    styling backgroung and adding inner cercle

--- src/App.js
+++ src/App.js
@@ -17,46 +17,53 @@ function App() {
   return (
     
         <div className="container">
-          <div className="wrapper">
-            <CircularProgressbarWithChildren
-                value={progress}
-                text={`${number}$`}
-                styles={{
-                  root: {},
-                  // Customize the path, i.e. the "completed progress"
-                  path: {
-                    // Path color
-                    stroke: `#7938ba`,
-                    // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-                    strokeLinecap: 'round',
-                    // Customize transition animation
-                    transition: 'stroke-dashoffset 0.5s ease 0s',
-                    // Rotate the path
-                    transform: 'rotate(0.0turn)',
-                    transformOrigin: 'center center',
-                  },
-                  // Customize the circle behind the path, i.e. the "total progress"
-                  trail: {
-                    // Trail color
-                    stroke: '#1b1c34',
-                    // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-                    strokeLinecap: 'round',
-                    // Rotate the trail
-                    transform: 'rotate(0.25turn)',
-                    transformOrigin: 'center center',
-                  },
-                  // Customize the text
-                  text: {
-                    // Text color
-                    fill: '#f88',
-                    // Text size
-                    fontSize: '25px',
-                  },
-                  // Customize background - only used when the `background` prop is true
-                  background: {
-                    fill: '#FFF',
-                  },
-                }}/>   
+          <div className="box">
+            <div className="wrapper">
+              <CircularProgressbarWithChildren
+                  value={progress}
+                  text={`${number}$`}
+                  strokeWidth={10}
+                  background={true}
+                  styles={{
+                    path: {
+                      stroke: `#7938ba`,
+                      strokeLinecap: 'round',
+                      // Customize transition animation
+                      transition: 'stroke-dashoffset 0.5s ease 0s',
+                      // Rotate the path
+                      transform: 'rotate(0.0turn)',
+                      transformOrigin: 'center center',
+                    },
+                    // Customize the circle behind the path, i.e. the "total progress"
+                    trail: {
+                      stroke: '#1b1c34',
+                      strokeLinecap: 'round',
+                      // Rotate the trail
+                      transform: 'rotate(0.25turn)',
+                      transformOrigin: 'center center',
+                    },
+                    // Customize the text
+                    text: {
+                      // Text color
+                      fill: '#f88',
+                      // Text size
+                      fontSize: '25px',
+                    },
+                    // Customize background - only used when the `background` prop is true
+                    background: {
+                      fill: '#FFF',
+                    },
+                  }}>
+                    <div style={{ width: "70%" }}>
+                      <CircularProgressbar
+                        strokeWidth={1}
+                        styles={buildStyles({
+                          pathColor: "rgba(121, 56, 186, 0.05)",
+                          })}
+                      />
+                    </div>
+                  </CircularProgressbarWithChildren>   
+            </div>
           </div>
         </div>
   );
