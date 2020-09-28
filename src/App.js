7996d9f2001cb805244137dc28e9a90d1523639c
Romain BOBOE <rboboe@gmail.com>
2020-09-28 17:49:14 -0400

    styling and first library config

--- src/App.js
+++ src/App.js
@@ -1,43 +1,66 @@
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
 import { CircularProgressbar, 
          CircularProgressbarWithChildren, 
          buildStyles
         } from 'react-circular-progressbar';
-
+import {CircleProgress} from 'react-gradient-progress'
 import 'react-circular-progressbar/dist/styles.css';
 import './App.css';
 
 
+
 function App() {
-  const percentage = 66;
+  const [progress, setProgress] = useState(10)
+  const [number, setNumber] = useState(10)
+
+
+
   return (
-  
-    <CircularProgressbar
-      value={percentage}
-      text={`${percentage}%`}
-      styles={buildStyles({
-        // Rotation of path and trail, in number of turns (0-1)
-        rotation: 0.25,
-     
-        // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-        strokeLinecap: 'butt',
-     
-        // Text size
-        textSize: '16px',
-     
-        // How long animation takes to go from one percentage to another, in seconds
-        pathTransitionDuration: 0.5,
-     
-        // Can specify path transition in more detail, or remove it entirely
-        // pathTransition: 'none',
-     
-        // Colors
-        pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
-        textColor: '#f88',
-        trailColor: '#d6d6d6',
-        backgroundColor: '#3e98c7'
-      })}
-    />
+    
+        <div className="container">
+          <div className="wrapper">
+            <CircularProgressbarWithChildren
+                value={progress}
+                text={`${number}$`}
+                styles={{
+                  root: {},
+                  // Customize the path, i.e. the "completed progress"
+                  path: {
+                    // Path color
+                    stroke: `#7938ba`,
+                    // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                    strokeLinecap: 'round',
+                    // Customize transition animation
+                    transition: 'stroke-dashoffset 0.5s ease 0s',
+                    // Rotate the path
+                    transform: 'rotate(0.0turn)',
+                    transformOrigin: 'center center',
+                  },
+                  // Customize the circle behind the path, i.e. the "total progress"
+                  trail: {
+                    // Trail color
+                    stroke: '#1b1c34',
+                    // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                    strokeLinecap: 'round',
+                    // Rotate the trail
+                    transform: 'rotate(0.25turn)',
+                    transformOrigin: 'center center',
+                  },
+                  // Customize the text
+                  text: {
+                    // Text color
+                    fill: '#f88',
+                    // Text size
+                    fontSize: '25px',
+                  },
+                  // Customize background - only used when the `background` prop is true
+                  background: {
+                    fill: '#FFF',
+                  },
+                }}/>   
+          </div>
+        </div>
   );
 }
 
